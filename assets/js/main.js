b388499b68143b9dd17b824fe3e6598f170bf89c
Ben McLaren <benmclaren@live.co.uk>
2020-12-10 16:49:29 +0000

    responsive navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,25 @@
-//    (function(){
-//         new InstagramFeed({
-//             'username': 'camillafitness_pt',
-//             'container': document.getElementById("instagram-feed"),
-//             'display_profile': false,
-//             'display_biography': false,
-//             'display_gallery': true,
-//             'display_captions': false,
-//             'callback': null,
-//             'styling': true,
-//             'items': 4,
-//             'items_per_row': 4,
-//             'margin': 1,
-//             'lazy_load': true,
-//             'on_error': console.error
-//         });
-//     })();
+   (function(){
+        new InstagramFeed({
+            'username': 'camillafitness_pt',
+            'container': document.getElementById("instagram-feed"),
+            'display_profile': false,
+            'display_biography': false,
+            'display_gallery': true,
+            'display_captions': false,
+            'callback': null,
+            'styling': true,
+            'items': 4,
+            'items_per_row': 4,
+            'margin': 1,
+            'lazy_load': true,
+            'on_error': console.error
+        });
+    })();
 
+
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('toggle');
+  console.log('hi');
+});
